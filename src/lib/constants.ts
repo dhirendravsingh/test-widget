de0ab037f2bfd7a8a1176d6ac7189d1da63207d6
dhirendravsingh <dhirendravsingh09@gmail.com>
2025-12-10 12:10:32 +0530

    d4

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -6,7 +6,7 @@ export const MAIN_API_BASE_URL = process.env.MAIN_API_BASE_URL
   ? process.env.MAIN_API_BASE_URL
   : isDev
   ? "https://185b-27-7-18-129.ngrok-free.app"
-  : "https://s7ctgw-ip-192-140-152-83.tunnelmole.net";
+  : "https://cdself-ip-27-7-22-198.tunnelmole.net";
 
 export const CHAT_API_BASE_URL = process.env.CHAT_API_BASE_URL
   ? process.env.CHAT_API_BASE_URL
